e6c9aa61d0b88651bb413c5fa7327ac229b02189
ae.whitemamba <ae.whitemamba@github.com>
2023-01-28 02:42:27 +0900

    first-vue-portfolio

---
+++ first-portfolio/src/App.vue
@@ -0,0 +1,159 @@
+<template>
+  <header>
+    <div class="header-box">
+      <div class="home">My</div>
+      <div class="header-wrap">
+        <div class="blog">blog</div>
+        <div class="git">git</div>
+        <div class="contact">contact me</div>
+      </div>
+    </div>
+  </header>
+  <div class="wrap">
+      <div class="profile">
+        <img src="./assets/avata.png" alt="남색의 비니를 쓴 반팔티를 입은 사람">
+      </div>
+      <main class="container">
+      <p>Hello 👋 I'm</p>
+      <section class="animation">
+        <div class="first"><div>ae.uj</div></div>
+        <div class="second"><div>Web Developer</div></div>
+        <div class="third"><div>Front-end Developers</div></div>
+      </section>
+    </main>
+  </div>
+</template>
+
+<script>
+import data from './assets/info.js';
+data;
+
+export default {
+  name: 'App',
+  data(){
+    return{
+
+    }
+  },
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+@import url('https://fonts.googleapis.com/css?family=Roboto:700');
+@font-face {
+    font-family: 'KyoboHandwriting2021sjy';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2212@1.0/KyoboHandwriting2021sjy.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+}
+
+body {
+  font-family:'Roboto';
+  background-color: #181D31;  
+  margin: 0;
+  padding: 0;
+}
+header{
+  display: flex;
+  margin: 0 auto;
+  padding: 20px;
+  position: relative;
+  background-color: #678983;
+  font-family: 'KyoboHandwriting2021sjy';
+}
+.header-box{
+  display: flex;
+  width: 80%;
+  margin: 0 auto;
+  position: relative;
+  padding: 30px;
+  font-size: 30px;
+}
+.header-wrap{
+  display: flex;
+  position: absolute;
+  right: 0;
+}
+.header-wrap > div{
+  margin-left: 40px;
+  cursor: pointer;
+  color: #F0E9D2;
+}
+.home{
+  color: #E6DDC4;
+  cursor: pointer;
+}
+.wrap{
+  display: flex;
+  width: 80%;
+  margin: 0 auto;
+  padding: 50px;
+  margin-top: 40px;
+}
+.profile img{
+  border-radius: 20px;
+  box-shadow: blue 0px 0px 0px 2px inset, rgb(255, 255, 255) 10px -10px 0px -3px, rgb(31, 193, 27) 10px -10px, rgb(255, 255, 255) 20px -20px 0px -3px, rgb(255, 217, 19) 20px -20px, rgb(255, 255, 255) 30px -30px 0px -3px, rgb(255, 156, 85) 30px -30px, rgb(255, 255, 255) 40px -40px 0px -3px, rgb(255, 85, 85) 40px -40px;
+}
+p {
+  text-shadow: 0 0 7px rgba(255,255,255,.3), 0 0 3px rgba(255,255,255,.3);
+}
+.container {
+  color: #e5e5e5;
+  font-size: 2.26rem;
+  text-transform: uppercase;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 50px;
+}
+
+.animation {
+  height:50px;
+  overflow:hidden;
+  margin-left: 1rem;
+}
+
+.animation > div > div {
+  padding: 0.25rem 0.75rem;
+  height:2.81rem;
+  margin-bottom: 2.81rem;
+  display:inline-block;
+}
+
+.animation div:first-child {
+  animation: text-animation 8s infinite;
+}
+
+.first div {
+  background-color:#678983;
+}
+.second div {
+  background-color:#CD921E;
+}
+.third div {
+  background-color:#181D31;
+}
+
+@keyframes text-animation {
+  0% {margin-top: 0;}
+  10% {margin-top: 0;}
+  20% {margin-top: -5.62rem;}
+  30% {margin-top: -5.62rem;}
+  40% {margin-top: -11.24rem;}
+  60% {margin-top: -11.24rem;}
+  70% {margin-top: -5.62rem;}
+  80% {margin-top: -5.62rem;}
+  90% {margin-top: 0;}
+  100% {margin-top: 0;}
+}
+</style>
---
+++ first-portfolio/src/assets/info.js
@@ -0,0 +1,23 @@
+export default
+[{
+  id: 0,
+  title: "Tistory blog",
+  image:"https://w.namu.la/s/ce4aadea7b43f47f9e3dcc67ab5c8ccdb670ae055d76ecb61d83511ef8cd531e5008529e86bf236bd1e60f1eaa351830725efdbd56a68c516abe96adc3a5c9bdc5b525cb39925d5fa22a193d52ffdf0ab4517adcbcf2b407000577a2134a07b7",
+  content : "html, css, javascript 풀이 위주",
+  important : "주목할 카테고리 : 트러블 슈팅"
+},
+{
+  id: 1,
+  title: "github",
+  image:"https://w.namu.la/s/a50a10b0fc00aaa6d9c384e5bdcdc8b791978a45d5864752cb3131e9d6a40dda44b9102f2223390959c5c5ae20523b8512f04d824dbf2e3e3e395f3fcf26010b99a42e9b25cc5adf6b1cba78733a38282b9dbaf8d97efc8cd8d3454dfb21dcbe",
+  content : "clone cording 위주",
+  important : "주목할 레포 : 연동된 웹 사이트"
+},
+{
+  id: 2,
+  title: "contact me",
+  image:"https://tistory1.daumcdn.net/tistory/5269774/attach/e7cb5772c5fe4409a8ef3ae1e1206ba3",
+  content : "연락할 방법",
+  important : "blog, github"
+},
+]
