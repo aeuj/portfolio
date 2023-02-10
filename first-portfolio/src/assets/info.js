030b12aaeb4eb6738f489875be01fe07f8ae85c3
ae.whitemamba <ae.whitemamba@github.com>
2023-02-11 03:23:18 +0900

    잘모르겠는데 pull 하라고 해서 해봄

--- first-portfolio/src/assets/info.js
+++ first-portfolio/src/assets/info.js
@@ -2,14 +2,14 @@ export default
 [{
   id: 0,
   title: "Tistory blog",
-  image:"https://w.namu.la/s/ce4aadea7b43f47f9e3dcc67ab5c8ccdb670ae055d76ecb61d83511ef8cd531e5008529e86bf236bd1e60f1eaa351830725efdbd56a68c516abe96adc3a5c9bdc5b525cb39925d5fa22a193d52ffdf0ab4517adcbcf2b407000577a2134a07b7",
+  image:"https://w.namu.la/s/ce4aadea7b43f47f9e3dcc67ab5c8ccdb670ae055d76ecb61d83511ef8cd531e5008529e86bf236bd1e60f1eaa351830725efdbd56a68c516abe96adc3a5c9bdc5b525cb39925d5fa22a193d52ffdf0adc3081e5f9152cbbeee0feb319bb7f4a",
   content : "html, css, javascript 풀이 위주",
   important : "주목할 카테고리 : 트러블 슈팅"
 },
 {
   id: 1,
   title: "github",
-  image:"https://w.namu.la/s/a50a10b0fc00aaa6d9c384e5bdcdc8b791978a45d5864752cb3131e9d6a40dda44b9102f2223390959c5c5ae20523b8512f04d824dbf2e3e3e395f3fcf26010b99a42e9b25cc5adf6b1cba78733a38282b9dbaf8d97efc8cd8d3454dfb21dcbe",
+  image:"https://w.namu.la/s/afd34d4c2d3dd15287fdd08ce10ce1f4dc6c153e0bc82300f4a143ffc79c930fd88ef850d7ee763314b87d7c710dd61ceea0eb4591741f668f8091847a7854dd71a00f2358355bd1f3c45e0d2db95cf3c16a32a9865553d5be81ec6a720367ad",
   content : "clone cording 위주",
   important : "주목할 레포 : 연동된 웹 사이트"
 },
--- first-portfolio/src/assets/readme.md
+++ first-portfolio/src/assets/readme.md
@@ -1,1 +1,43 @@
 # 처음으로 vue를 이용해서 만들어본 mini-portfolio
+
+<h3>vue3 설치 </h3>
+<strong>window 기준</strong>  
+```
+npm install -g @vue/cli
+```
+vue2 또는 vue3 선택
+
+```
+ vue create 프로젝트명 
+```
+
+<p>프로젝트명으로 파일을 다시 열고</p>
+```
+npm run serve
+```
+터미널에 뜨는 링크로 창을 연다
+
+<h4>기본사항</h4>
+<ul>
+  <li>assets 에 이미지</li>
+  <li>assets 에 js파일(data담음)</li>
+  <li>components에 vue파일</li>
+  <li>components에 단어 2글자 안만들고 싶으면 package.json에 rule 부분에 추가문법 요망</li>
+  <li>app.vue 파일로 시작</li>
+</ul>  
+
+
+<h4>기능</h4>
+<ul>
+  <li>이미지 hover</li>
+  <li>nav menu 정렬 export default로 받아오기</li>
+  <li>nav menu click시 data자동으로 변경</li>
+</ul>  
+
+
+<h4>next portfolio</h4>
+<ul>
+  <li>components 사용</li>
+</ul>  
+<i>피그마로 스케치 해봄(구성)</i>
+[참고animaiton](https://codepen.io/carloscdev/pen/mdBLmwP)
